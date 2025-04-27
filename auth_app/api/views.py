e3afd5819c1233f56761a7bf819b5fe3cc4467da
Linda Österreicher <lindaoest@gmail.com>
2025-04-27 18:21:04 +0200

    Create basic views for later filling

---
+++ auth_app/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+
+class RegistrationView(generics.ListCreateAPIView):
+	pass
+
+class LoginView(generics.ListCreateAPIView):
+	pass
+
+class ProfileView(generics.ListCreateAPIView):
+	pass
+
+class ProfileBusinessView(generics.ListCreateAPIView):
+	pass
+
+class ProfileCustomerView(generics.ListCreateAPIView):
+	pass
--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets
 
 # Create your views here.
-class OfferView(generics.ListCreateAPIView):
+class OfferViewset(viewsets.ModelViewSet):
+	pass
+
+class OfferDetailView(generics.RetrieveUpdateDestroyAPIView):
+	pass
+
+class OrderViewset(viewsets.ModelViewSet):
+	pass
+
+class OrderCountView(generics.ListAPIView):
+	pass
+
+class CompletedOrderCount(generics.ListAPIView):
+	pass
+
+class ReviewViewset(viewsets.ModelViewSet):
+	pass
+
+class BaseInfoView(generics.ListAPIView):
 	pass
