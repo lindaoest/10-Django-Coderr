e47673d2bb2bccf9503352a5cd9551272bde9677
Linda Österreicher <lindaoest@gmail.com>
2025-04-27 18:20:13 +0200

    Update Models

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,21 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+TYPECHOICES = [
+	("business", "Business"),
+	("customer", "Customer")
+]
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+	username = models.CharField(max_length=150)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	file = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+	type = models.CharField(max_length=50, choices=TYPECHOICES, default='customer')
+	email = models.EmailField()
 
 class CustomerProfile(models.Model):
-	first_name = models.CharField(max_length=155)
-	last_name = models.CharField(max_length=155)
-	username = models.CharField(max_length=155)
-	email = models.EmailField(max_length=366, unique=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	file = models.FileField(upload_to='profile/customer/')
+	user_profile = models.OneToOneField(UserProfile, related_name="customer_profile", on_delete=models.CASCADE)
+	uploaded_at = models.DateTimeField(auto_now_add=True)
 
 class BusinessProfile(models.Model):
-	first_name = models.CharField(max_length=155)
-	last_name = models.CharField(max_length=155)
-	username = models.CharField(max_length=155)
+	user_profile = models.OneToOneField(UserProfile, related_name="business_profile", on_delete=models.CASCADE)
+	location = models.CharField(max_length=255)
+	tel = models.CharField(max_length=255)
 	description = models.TextField()
-	email = models.EmailField(max_length=366, unique=True)
+	working_hours = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
-	file = models.FileField(upload_to='profile/business/')
-	tel = models.IntegerField()
-	location = models.CharField(max_length=366)
-	working_hours = models.CharField(max_length=366)
--- coderr_app/models.py
+++ coderr_app/models.py
@@ -8,43 +8,65 @@ STATUS_CHOICES = [
 ]
 
 TYPECHOICES = [
-	("base", "Base"),
+	("basic", "Basic"),
 	("standard", "Standard"),
 	("premium", "Premium")
 ]
 
 class Offer(models.Model):
-	title = models.CharField(max_length=366)
-	text = models.TextField(blank=True)
-	createdAt = models.DateTimeField(auto_now_add=True)
-	image = models.FileField(upload_to='single_offer/')
-	business_user = models.ForeignKey(BusinessProfile, related_name='offer', on_delete=models.CASCADE)
-
-class OfferOption(models.Model):
-	title = models.CharField(max_length=366)
-	price = models.FloatField()
-	offer_type = models.CharField(choices=TYPECHOICES, default='base')
-	delivery_time_in_days = models.CharField()
-	revisions = models.CharField(max_length=155)
-	list = models.TextField()
-	features = models.JSONField()
-	offer = models.ForeignKey(Offer, related_name='offerOption', on_delete=models.CASCADE)
-
-class Order(models.Model):
-	status = models.CharField(choices=STATUS_CHOICES, default='in_progress')
-	createdAt = models.DateTimeField(auto_now_add=True)
-	offer_detail_id = models.ForeignKey(Offer, related_name='orders', on_delete=models.CASCADE)
-	customer_user = models.ForeignKey(CustomerProfile, related_name='orders', on_delete=models.CASCADE)
-	business_user = models.ForeignKey(BusinessProfile, related_name='orders', on_delete=models.CASCADE)
-
-
-class Review(models.Model):
+	user = models.ForeignKey(BusinessProfile, related_name='offers', on_delete=models.CASCADE)
+	title = models.CharField(max_length=255)
+	image = models.ImageField(upload_to='offers/', null=True)
 	description = models.TextField(blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
-	rating = models.IntegerField()
-	offer = models.ForeignKey(Offer, related_name='reviews', on_delete=models.CASCADE)
-	reviewer = models.ForeignKey(CustomerProfile, related_name='reviews', on_delete=models.CASCADE)
+	updated_at = models.DateTimeField(auto_now_add=True)
+	min_price = models.DecimalField(decimal_places=2)
+	min_delivery_time = models.IntegerField()
+
+class OfferDetail(models.Model):
+	title = models.CharField(max_length=255)
+	revisions = models.IntegerField(max_length=255)
+	delivery_time_in_days = models.IntegerField()
+	price = models.DecimalField(decimal_places=2)
+	features = models.JSONField()
+	offer_type = models.CharField(choices=TYPECHOICES, default='basic')
+	# list = models.TextField()
+	offer = models.ForeignKey(Offer, related_name='details', on_delete=models.CASCADE)
+
+class Order(models.Model):
+	customer_user = models.ForeignKey(CustomerProfile, related_name='orders', on_delete=models.CASCADE)
+	business_user = models.ForeignKey(BusinessProfile, related_name='orders', on_delete=models.CASCADE)
+	title = models.CharField(max_length=255)
+	revisions = models.IntegerField()
+	delivery_time_in_days = models.IntegerField()
+	price = models.DecimalField(decimal_places=2)
+	features = models.JSONField()
+	offer_type = models.CharField(choices=TYPECHOICES, default='basic')
+	status = models.CharField(choices=STATUS_CHOICES, default='in_progress')
+	# list = models.TextField()
+	# offer = models.ForeignKey(Offer, related_name='offerOption', on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+	# offer_detail_id = models.ForeignKey(Offer, related_name='orders', on_delete=models.CASCADE)
+
+class OrderCount(models.Model):
+	order_count = models.IntegerField()
+
+class CompletedOrderCount(models.Model):
+	completed_order_count = models.IntegerField()
+
+class Review(models.Model):
 	business_user = models.ForeignKey(BusinessProfile, related_name='reviews', on_delete=models.CASCADE)
+	reviewer = models.ForeignKey(CustomerProfile, related_name='reviews', on_delete=models.CASCADE)
+	rating = models.DecimalField(decimal_places=2)
+	description = models.TextField(blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+	# offer = models.ForeignKey(Offer, related_name='reviews', on_delete=models.CASCADE)
 
-
+class BaseInfo(models.Model):
+	review_count = models.IntegerField()
+	average_rating = models.DecimalField(max_digits=3, decimal_places=2)
+	business_profile_count = models.IntegerField()
+	offer_count = models.IntegerField()
 
